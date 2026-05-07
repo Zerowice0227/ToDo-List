a46f7a2845e964f81deae0f4a1f7c58084b83da8
xayitboyev ziyodulla <steam163200227@gmail.com>
2026-05-07 17:41:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 // Kerakli HTML tag lariga ulanish
 
-const form = document.querySelector(".content_form");
+const form = document.querySelector(".form");
 const input = document.querySelector(".form_input");
-const addBtn = document.querySelector(".form_btn");
+const btn = document.querySelector(".add_btn");
 const todolist = document.querySelector(".todo_list");
 
-
 form.addEventListener("submit", function (e) {
-console.log("test"); // qayta yuklanishni o'chiradi
-
- if (!input.value.trim()) { 
-     // input.value - input ICIDAGI  qiymatni olish
+  e.preventDefault(); // qayta yuklanish hodisasini o'chiradi
+  // input.value - input ichidagi qiymatni olish 
+  
+  // Agar input ichida space dan boshqa narsa yo'q bo'lsa, dastur to'xtashi kerak
+  if (!input.value.trim()) {
     return;
-  }
- // classlist - html elementlariga class qo'shish, o'chirish uchun ishlatiladi
-  // innerHTML - js o'zgaruvchilari ichiga html code qo'shish
-   const list_item = document.createElement("div");
-  list_item.classList.add("item");
-  list_item.innerHTML = `
-   <div class="list_info">
-        <input class="checkbox" type="checkbox">
+  };
+
+    // list_item class li div yasash kerak
+    const item = document.createElement('div')
+    // classlist - html elementiga class qo'shish, o'chirish uchun ishlatiladi
+    // innerHTML - js o'zgaruvchilari ichiga HTML kod qo'shadi
+    item.classList.add('list_item');
+    item.innerHTML = `
+    <div class="item_info">
+        <input type="checkbox" class="checkbox">
         <p class="todo">${input.value}</p>
     </div>
     <button class="remove_btn">
-        <img src="ToDo List/img/trash.bin.svg" width="20" height="20" alt="trash-icon">
+        <img src="./img/trash.svg" width="50" height="50" alt="trash-icon">
     </button>
-  `
+    `
 
-const checkbox = item.querySelector(".checkbox");
-const removeBtn = item.querySelector(".todo");
+    const checkbox = item.querySelector('.checkbox')
+    const todo = item.querySelector('.todo')
+    const remove_btn = item.querySelector('.remove_btn')
 
-checkbox.addEventListener("change", function (e) {
- todo.style.textDecoration = checkbox.checked ? "line-through" : "none";
-}) 
+    checkbox.addEventListener('change', function(e)  {
+        todo.style.textDecoration = checkbox.checked ? 'line-through' : 'none'
+    })
 
-removeBtn.addEventListener("click", () => {
-    item.remove();
-})
+    remove_btn.addEventListener('click', () => {
+        item.remove()
+    })
 
-
-
-
+    todolist.appendChild(item);
+    input.value = ''
 
 });
